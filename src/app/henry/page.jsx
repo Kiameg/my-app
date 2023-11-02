563c3b6630013c7ab796e319ec4b9f122f936081
Kia Meggele <kia1262@stud.kea.dk>
2023-11-02 11:47:47 +0100

    hkh

--- src/app/henry/page.jsx
+++ src/app/henry/page.jsx
@@ -7,14 +7,26 @@ export async function generateMetadata() {
   };
 }
 
+import Image from "next/image";
+
 export default async function Henry() {
   const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
   const data = await res.json();
   const { name, favouriteColor, age, image } = data;
+  console.log(image);
 
   return (
-    <main>
-      <h1>{name}</h1>
-    </main>
+    <Image
+      className="w-auto h-auto"
+      src="https://images.unsplash.com/photo-1517849845537-4d257902454a"
+      alt="A cute dog"
+      width={3024}
+      height={4032}
+      priority={true}
+      sizes="(max-width: 768px) 100vw,
+         (max-width: 1200px) 50vw,
+         400px"
+    />
+    // disables lazy load
   );
 }
